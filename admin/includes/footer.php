2e51dc2e5f850a444f347a73110460f39e6deca5
Motabo <motabo@kali.kali.org>
2024-07-29 21:11:37 +0200

    First commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2023 Brought To You By<a href="thuto.nul.ls/">Mapeshoane, Ntilane & Hlaha</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2023 Brought to You By <a href="https://www.google.com/search?client=firefox-b-e&q=pholoho+Hlaha#ip=1">Mapeshoane, Ntilane & Hlaha</a></strong>
+    </div>
+</footer>
